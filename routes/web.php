eca1db00fc9f79d3f7d972ac50690caa796a7b2f
alvindioprakosa <alvindioprakosa@gmail.com>
2025-02-07 11:17:45 +0700

    Update route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+// Redirect dari '/' ke '/home'
+Route::redirect('/', '/home');
+
+// Route untuk '/home'
+Route::get('/home', [HomeController::class, 'index']);
