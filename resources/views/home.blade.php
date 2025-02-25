12603c4cb4ed86c1273657e9c4c6b9fa3f24267f
alvindioprakosa <alvindioprakosa@gmail.com>
2025-02-25 11:55:48 +0700

    Update Name Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 <div class="max-w-sm mx-auto bg-white rounded-lg shadow-lg overflow-hidden w-full">
                     <div class="p-4">
-                        <h2 class="text-xl font-semibold text-gray-800">Connie Kwan, SparkAI</h2>
+                        <h2 class="text-xl font-semibold text-gray-800">Alvin Dio, SparkAI</h2>
                         <p class="text-gray-600 mt-2">List of Numbers {{json_encode($listOfInteger)}}</p>
                         <p class="text-gray-600 mt-2">Minimum Number {{$min}}</p>
                         <p class="text-gray-600 mt-2">Maximum Number {{$max}}</p>
