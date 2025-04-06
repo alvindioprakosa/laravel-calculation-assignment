34f4aa9c518ad17fbe7dcab078152997b3010972
alvindioprakosa <alvindioprakosa@gmail.com>
2025-04-06 21:14:01 +0700

    Update Home Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,78 +7,28 @@ use Illuminate\Support\Arr;
 
 class HomeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $calculate = $this->calculate([1, 5, 3, 2, 5, 2, 1, 1]);
+        $numbers = [1, 5, 3, 2, 5, 2, 1, 1];
+        $result = $this->calculate($numbers);
 
-        return view('home', $calculate);
+        return view('home', $result);
     }
 
-    public function calculate($listOfInteger)
+    protected function calculate(array $numbers): array
     {
-        $min = min($listOfInteger);
-        $max = max($listOfInteger);
-        $odd = Arr::where($listOfInteger, function ($value, $key) {
-            return $value % 2 !== 0;
-        });
-        $odd = array_values($odd);
+        $min = min($numbers);
+        $max = max($numbers);
+        $odd = array_values(array_filter($numbers, fn($num) => $num % 2 !== 0));
 
-        return compact(
-            'listOfInteger',
-            'min',
-            'max',
-            'odd'
-        );
+        return compact('numbers', 'min', 'max', 'odd');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+    // Resource methods below can be removed if not used
+    public function create() {}
+    public function store(Request $request) {}
+    public function show(string $id) {}
+    public function edit(string $id) {}
+    public function update(Request $request, string $id) {}
+    public function destroy(string $id) {}
 }
