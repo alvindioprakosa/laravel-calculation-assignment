cf9ddda5e59a125a4e7b143971ae58e5b07b268f
alvindioprakosa <alvindioprakosa@gmail.com>
2025-04-28 10:54:55 +0700

    Update Route Console

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+// Membuat command 'php artisan inspire'
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+    $this->comment(Inspiring::quote()); // Menampilkan quote inspiratif
+})->purpose('Display an inspiring quote')->hourly(); // Kasih deskripsi & buat dijadwalkan tiap jam
